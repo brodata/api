3acfee4a4918431fde5782d8adcdde7db6d5e09a
Mikhail Movchan <devcore.ru@gmail.com>
2020-09-21 08:50:40 +0300

    refactorinf CitiesController.cs

--- BroData.API/Controllers/v0/CitiesController.cs
+++ BroData.API/Controllers/v0/CitiesController.cs
@@ -1,6 +1,9 @@
-﻿using BroData.API.Models.v0;
+﻿using BroData.API.Data;
+using BroData.API.Models.v0;
 using BroData.API.Service.v0;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BroData.API.Controllers.v0
@@ -9,14 +12,7 @@ namespace BroData.API.Controllers.v0
     [ApiController]
     public class CitiesController : ControllerBase
     {
-        private readonly ICityService _cityService;
-        private readonly IGetCallCounterService _getCallCounterService;
 
-        public CitiesController(ICityService cityService, IGetCallCounterService getCallCounterService)
-        {
-            _cityService = cityService;
-            _getCallCounterService = getCallCounterService;
-        }
 
         /// <summary>
         /// Gets the list of all US Cities.
@@ -24,30 +20,25 @@ namespace BroData.API.Controllers.v0
         /// <returns>The list of City.</returns>
         // GET: api/Employee
         [HttpGet]
-        async public Task<IActionResult> GetAll()
-        {
-            await _getCallCounterService.UpdateCounter(HttpContext);
-            return await Task.FromResult(Ok(new Response<ICity>(await _cityService.GetAll())));
-        }
+        public IEnumerable<ICity> GetAll() => CitiesRepo.GetAll();
+
         /// <summary>
         /// Gets by State the list of all US Cities.
         /// </summary>
         /// <returns>The list of City.</returns>
         [HttpGet("{state}")]
-        async public Task<IActionResult> GetByState(string state)
+        public IEnumerable<ICity> GetByState(string state)
         {
-            await _getCallCounterService.UpdateCounter(HttpContext);
-            return await Task.FromResult(Ok(new Response<ICity>(await _cityService.GetByState(state))));
+            return CitiesRepo.GetAll().Where(x => x.state_code == state);
         }
         /// <summary>
         /// Gets by ID the of US Cities.
         /// </summary>
         /// <returns>The item of City.</returns>
         [HttpGet("{id:int}")]
-        async public Task<IActionResult> GetByID(int id)
+        public ICity GetByID(int id)
         {
-            await _getCallCounterService.UpdateCounter(HttpContext);
-            return await Task.FromResult(Ok(new Response<ICity>(await _cityService.GetByID(id))));
+            return CitiesRepo.GetAll().Where(x => x.id == id).First();
         }
     }
 }
